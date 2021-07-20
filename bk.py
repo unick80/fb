54e66d9586ce3d153d49ecd1dacd282ff1786740
Nick <zagis-net@mail.ru>
2021-07-20 20:58:41 +0300

    first commit

---
+++ bk.py
@@ -0,0 +1,52 @@
+import json
+import requests
+import hashlib
+import uuid
+from selenium import webdriver
+from time import sleep
+import random
+
+response = requests.get('https://line11.bkfon-resources.com/live/currentLine/ru')
+try:
+  json = response.json()
+except:
+  json="Error parser json"
+  exit()
+else:
+  announcements = json['announcements']
+  gamers = {}
+  for i in range(len(announcements)):
+    try:
+      team1 = announcements[i]['team1']
+    except:
+      team1 = ""
+    try:
+      team2 = announcements[i]['team2']
+    except:
+      team2 = ""
+    id = hashlib.md5(str(uuid.uuid4()).encode()).hexdigest()
+    gamer_name = team1+" - "+team2
+    gamers[id] = gamer_name
+
+driver = webdriver.Chrome()
+driver.get("https://www.fonbet.ru/live/")
+sleep(5)
+btn_search = driver.find_element_by_class_name("search")
+btn_search.click()
+input_search = driver.find_element_by_class_name("search__input--DF661")
+ok = 0
+while ok==0:
+  key = random.choice(list(gamers.keys()))
+  gamer_name = gamers[key]
+  driver.execute_script('arguments[0].value = "";', input_search)
+  input_search.send_keys(gamer_name)
+  print(gamer_name)
+  sleep(5)
+  try:
+    result_search = driver.find_element_by_class_name("search-result__event-name--3Qfnn")
+  except:
+    pass
+  else:
+    ok=1
+    result_search.click()
+  sleep(5)
